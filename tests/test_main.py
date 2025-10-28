119a452dcca4d02aab7b377c2bc2b6cf2ddd1d5a
Sidorov <sidordima@mail.com>
2025-10-28 18:55:30 +0400

    add config.yml.sample

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,5 +4,5 @@ from main import read_config
 
 class TestLoadFunctions(unittest.TestCase):
     def test_load_tasks(self):
-        result = read_config("config.yaml.sample")
+        result = read_config("config.yml.sample")
         self.assertIsInstance(result, dict)
